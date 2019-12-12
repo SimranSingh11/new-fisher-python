926c0291f9876175577eb9b31557d71f1068421b
shahnavaz <shahnavazv16@gmail.com>
2019-12-13 00:26:48 +0530

    update task list

--- task/serializers.py
+++ task/serializers.py
@@ -9,19 +9,23 @@ class TaskSerializer(serializers.ModelSerializer):
     assignee_id = serializers.IntegerField(required=False, allow_null=True)
     issue_date = serializers.DateTimeField(required=False, allow_null=True)
     due_date = serializers.DateTimeField(required=False, allow_null=True)
-    assignee_info = serializers.SerializerMethodField()
     is_active = serializers.BooleanField(read_only=True)
+    assignee_info = serializers.SerializerMethodField()
+    issue_date_timestamp = serializers.SerializerMethodField()
+    due_date_timestamp = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Task
-        fields = ('id', 'task', 'assignee_id', 'issue_date', 'due_date', 'assignee_info', 'is_active')
+        fields = ('id', 'task', 'assignee_id', 'issue_date', 
+                'due_date', 'assignee_info', 'is_active',
+                'issue_date_timestamp', 'due_date_timestamp')
 
     def to_internal_value(self, attr_data):
 
         from my_helpers.datetime_helper import timestamp_to_datetime
 
         # p_print("before attr_data", attr_data)
-        data = attr_data
+        data = attr_data.copy()
         issue_date = data.get('issue_date', None)
         due_date = data.get('due_date', None)
 
@@ -31,7 +35,7 @@ class TaskSerializer(serializers.ModelSerializer):
         if due_date:
             data['due_date'] = timestamp_to_datetime(due_date) 
 
-        # p_print("after data", data)
+        p_print("after data", data)
         return super().to_internal_value(data)
 
 
@@ -39,3 +43,11 @@ class TaskSerializer(serializers.ModelSerializer):
         from user.helpers import get_user_obj_data
         assignee_info = get_user_obj_data(obj.assignee) if obj.assignee else dict()
         return assignee_info
+
+    def get_issue_date_timestamp(self, obj):
+        from my_helpers.datetime_helper import datetime_to_timestamp
+        return datetime_to_timestamp(obj.issue_date)
+
+    def get_due_date_timestamp(self, obj):
+        from my_helpers.datetime_helper import datetime_to_timestamp
+        return datetime_to_timestamp(obj.due_date)
